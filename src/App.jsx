1d4f74af9156897ef78c5ad5af888790c1eaec67
Devuno-official <185803128+Yasircodingbt@users.noreply.github.com>
2025-10-11 19:50:52 +0500

    add one button for remove string

--- src/App.jsx
+++ src/App.jsx
@@ -45,20 +45,20 @@ export default function Calculator() {
 
   return (
     <Container>
-      <div className="w-full h-full sm:h-auto bg-zinc-900 rounded-none sm:rounded-3xl shadow-[0_0_20px_rgba(0,0,0,0.3)] overflow-hidden flex flex-col">
-        <div className="flex flex-col p-2 sm:p-3 md:p-4 flex-1">
+      <div className="w-full h-screen sm:h-auto bg-black rounded-none sm:rounded-3xl shadow-[0_0_20px_rgba(0,0,0,0.3)] overflow-hidden flex flex-col">
+        <div className="flex flex-col p-0 sm:p-3 md:p-4 flex-1">
           <input
             type="text"
             name="input"
             id="input"
-            className="w-full h-20 sm:h-24 md:h-28 px-3 sm:px-4 md:px-6 
-                                bg-zinc-900
-                                text-right text-white text-3xl sm:text-4xl md:text-5xl font-light
-                                border-none outline-none mb-2 sm:mb-3 md:mb-4"
+            className="w-full h-32 sm:h-24 md:h-28 px-6 sm:px-4 md:px-6 
+                                bg-black
+                                text-right text-white text-4xl sm:text-4xl md:text-5xl font-light
+                                border-none outline-none mb-0 sm:mb-3 md:mb-4"
             value={inputVal}
             readOnly
           />
-          <div className="grid grid-cols-4 gap-1 sm:gap-2 md:gap-3 flex-1">
+          <div className="grid grid-cols-4 gap-0 sm:gap-2 md:gap-3 flex-1">
             <Btn symbol={arr} showValHandle={showValHandle} />
           </div>
         </div>
